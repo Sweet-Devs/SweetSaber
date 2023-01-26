14093a8c655f41567b08f44e98d2b09887cdb3f5
Takumi-kun <css79113@gmail.com>
2023-01-26 17:46:46 +0300

    MainWindow stuff

--- SweetSaber/ViewModels/MainWindowViewModel.cs
+++ SweetSaber/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,25 @@
-﻿namespace SweetSaber.ViewModels
+﻿using System.Collections.Generic;
+using SweetSaber.Models;
+using System.Collections.ObjectModel;
+using ReactiveUI;
+
+namespace SweetSaber.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        private int _tabIndex;
+
+        public static List<VerticalTab> Tabs => new()
+        {
+            new VerticalTab { Name = "start" },
+            new VerticalTab { Name = "mods" },
+            new VerticalTab { Name = "settings" }
+        };
+
+        public int TabIndex
+        {
+            get => _tabIndex;
+            set => this.RaiseAndSetIfChanged(ref _tabIndex, value);
+        }
     }
 }
