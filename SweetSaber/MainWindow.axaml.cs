6b18878e279135264879187821688c2db87c497f
Takumi-kun <css79113@gmail.com>
2023-01-26 16:37:13 +0300

    Добавьте файлы проекта.

---
+++ SweetSaber/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SweetSaber
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
