919eff8efa6fef09b6ca7cb3c7cf0df86ee4ffdd
Takumi-kun <css79113@gmail.com>
2023-02-07 01:25:22 +0300

    Manual files drop-down in designated area

--- SweetSaber/Views/MainWindow.axaml.cs
+++ SweetSaber/Views/MainWindow.axaml.cs
@@ -1,17 +1,50 @@
 using Avalonia.Controls;
 using SweetSaber.ViewModels;
 using System;
+using System.Linq;
 using Avalonia.Input;
 using SweetSaber.BeatMods.Models;
 using SweetSaber.Models;
+using System.Reflection;
+using Avalonia.Interactivity;
 
 namespace SweetSaber.Views
 {
     public partial class MainWindow : Window
     {
+        TextBlock _DropState;
+        Border _DropBorder;
+        private const string CustomFormat = "application/xxx-avalonia-controlcatalog-custom";
+
         public MainWindow()
         {
             InitializeComponent();
+            _DropState = this.Get<TextBlock>("DropState");
+            _DropBorder = this.Get<Border>("DropBorder");
+
+            _DropBorder.AddHandler(DragDrop.DropEvent, Drop);
+            _DropBorder.AddHandler(DragDrop.DragEnterEvent, DraggingEnter);
+            _DropBorder.AddHandler(DragDrop.DragLeaveEvent, DraggingLeave);
+        }
+
+        private void DraggingLeave(object? sender, RoutedEventArgs e)
+        {
+            _DropBorder.Classes.Remove("Dragging");
+        }
+
+        private void DraggingEnter(object? sender, DragEventArgs e)
+        {
+            _DropBorder.Classes.Add("Dragging");
+        }
+
+        private void Drop(object? sender, DragEventArgs e)
+        {
+            if (DataContext is not MainWindowViewModel vm) return;
+            if (!e.Data.Contains(DataFormats.FileNames)) return;
+
+            var fileName = e.Data.GetFileNames()?.First();
+
+            vm.Status = $"Loading mod from: {fileName}";
         }
 
         protected override void OnOpened(EventArgs e)
@@ -59,5 +92,6 @@ namespace SweetSaber.Views
 
             }
         }
+
     }
 }
